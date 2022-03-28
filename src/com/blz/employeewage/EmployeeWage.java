9ac982d3b35d340d5894fb3f1f1c1b007c6e4f4c
Pranil Shende <pranilshende05@gmail.com>
2022-03-28 09:45:55 +0530

    Added:calculate the daily wage

--- src/com/blz/employeewage/EmployeeWage.java
+++ src/com/blz/employeewage/EmployeeWage.java
@@ -1,17 +1,21 @@
 package com.blz.employeewage;
 
 public class EmployeeWage {
-public static void main(String[] args) {
-	int isFULL_TIME = 1;
-    double empcheck = (int) (Math.random() * 100) % 2;
-    if (empcheck == isFULL_TIME)
-    
-        System.out.println("Employee is Present");
-
-     else
-    
-        System.out.println("Employee is Absent");
-
-    }
+	public static void main(String[] args, String Wage) {
+		int isFULL_TIME = 1;
+	    int Emp_Rate_Per_Hr = 20;
+	    int empcheck = (int) (Math.random() * 100) % 2;
+	    int workingHours = 0;
+	    if (empcheck == isFULL_TIME)
+	    
+	        System.out.println("Employee is Present");
+	        workingHours = 8;
+	    
+	    
+	        System.out.println("Employee is Absent");
+	    
+	    int dailywage = workingHours * Emp_Rate_Per_Hr;
+	    System.out.println("Employee Daily Wage is " + Wage);
+		}
+	
 }
-
